88d62df6d9e07eb55e21eea3bf37c35ba2070fa1
0122-0 <flowercats95@gmail.com>
2025-05-16 11:49:21 +0900

    "최초 생성"

---
+++ src/main/java/com/example/newdelivery/NewDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.newdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewDeliveryApplication.class, args);
+    }
+
+}
